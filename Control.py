ad46c77632059a338c0b558f953a5ed4d44b0b6e
Erik Borella <erikborella@gmail.com>
2019-11-11 12:08:02 -0300

    rename hello_world route to index

--- Control.py
+++ Control.py
@@ -5,11 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 from routes.auth import auth
 
 app = Flask(__name__)
+app.secret_key = "personificação"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/db'
 db = SQLAlchemy(app)
 
 app.register_blueprint(auth)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello World!'
