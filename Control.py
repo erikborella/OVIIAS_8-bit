ccb5245620bd35826a349014c1635a9172106346
Erik Borella <erikborella@gmail.com>
2019-11-11 17:06:04 -0300

    added store blueprint to control

--- Control.py
+++ Control.py
@@ -1,10 +1,12 @@
 from app import create_app
 import session
 from routes.auth import auth
+from routes.store import store
 
 app = create_app()
 
 app.register_blueprint(auth)
+app.register_blueprint(store)
 
 @app.route('/')
 def index():
