ccf7254723f078ba3b10241f59fae5c82fc83e4c
Erik Borella <erikborella@gmail.com>
2019-11-11 14:35:21 -0300

    added username verification and hash password

--- routes/auth.py
+++ routes/auth.py
@@ -4,7 +4,22 @@ from Forms_model import LoginForm, SingupForm
 from extensions import db
 from Models import User
 
+import hashlib
+
 auth = Blueprint('auth', __name__)
+
+
+def hash_string(string):
+    sha_signature = hashlib.sha256(string.encode()).hexdigest()
+    return sha_signature
+
+
+def check_existent_username(username):
+    users = User.query.all()
+    for user in users:
+        if user.username == username:
+            return True
+
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
@@ -18,16 +33,18 @@ def login():
 @auth.route('/singup', methods=['GET', 'POST'])
 def singup():
     form = SingupForm()
-
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
         email = form.email.data
 
-        new_user = User(username, password, email)
-        
-        db.session.add(new_user)
-        db.session.commit()
+        if (check_existent_username(username)):
+            return render_template("singup.html", form=form, errors=form.errors, error="nome de usuario ja existente")
+        else:
+            new_user = User(username, hash_string(password), email)
+
+            db.session.add(new_user)
+            db.session.commit()
 
         return redirect(url_for("index"))
 
