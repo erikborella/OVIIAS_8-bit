296addb607ea82f7f0a1a4a6906639fdf23aa2ae
Erik Borella <erikborella@gmail.com>
2019-11-13 08:36:01 -0300

    create route to add a product to cart

--- control.py
+++ control.py
@@ -2,8 +2,10 @@ from app import create_app
 import session
 from routes.auth import auth
 from routes.store import store
+from routes.cart import cart
 
 app = create_app()
 
 app.register_blueprint(auth)
 app.register_blueprint(store)
+app.register_blueprint(cart)
---
+++ routes/cart.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, redirect, url_for, flash
+from sqlalchemy import update
+from extensions import db
+
+from models import Product, User
+
+import session
+
+cart = Blueprint('cart', __name__)
+
+
+@cart.route("/cart/add/<id>/<int:quant>")
+def add_to_cart(id, quant):
+    if not session.is_logged():
+        flash("Voce precisa estar logado para fazer isso")
+        return redirect(url_for('auth.login'))
+    else:
+        product: Product = Product.query.filter_by(
+            id=id).first_or_404("Produto nao encontrado")
+        user: User = User.query.filter_by(
+            id=session.get()).first_or_404("Usuario invalido")
+
+        if product.stock <= 0 or quant > product.stock:
+            flash("Quantidade do produto indiponivel")
+            return redirect(url_for('store.product', id=id))
+        else:
+            user.n_items += quant
+            user.subtotal += product.price * quant
+
+            product.stock -= quant
+            
+            db.session.commit()
+            return "sim"
