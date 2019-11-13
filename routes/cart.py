3d2e8ef7a0255104117165a703b38fe7dfa0e072
Erik Borella <erikborella@gmail.com>
2019-11-13 09:31:33 -0300

    create remove route and fix add

--- routes/cart.py
+++ routes/cart.py
@@ -1,15 +1,15 @@
-from flask import Blueprint, redirect, url_for, flash
+from flask import Blueprint, redirect, url_for, flash, render_template
 from extensions import db
 
-from models import Product, User
+from models import Product, User, Buy
 
 import session
 
 cart = Blueprint('cart', __name__)
 
 
-@cart.route("/cart/add/<id>/<int:quant>")
-def add_to_cart(id, quant):
+@cart.route("/cart/add/<int:id>/<int:quant>")
+def add_to_cart(id: int, quant: int):
     if not session.is_logged():
         flash("Voce precisa estar logado para fazer isso")
         return redirect(url_for('auth.login'))
@@ -23,10 +23,48 @@ def add_to_cart(id, quant):
             flash("Quantidade do produto indiponivel")
             return redirect(url_for('store.product', id=id))
         else:
+            buy = Buy.query.filter_by(user_id=session.get(), product_id=id).first()
+
+            # Check if alredy have the buy with this product
+            if buy is None:
+                buy = Buy(user, product, quant)
+                db.session.add(buy)
+            else:
+                buy.n_items += quant
+
             user.n_items += quant
             user.subtotal += product.price * quant
 
             product.stock -= quant
 
             db.session.commit()
-            return "sim"
+
+            flash("Produto adicionado com sucesso ao carinho")
+            return redirect(url_for("store.showcase"))
+
+
+@cart.route("/cart/remove/<int:id>/<int:quant>")
+def remove_to_cart(id: int, quant: int):
+    if not session.is_logged():
+        flash("Voce precisa estar logado para fazer isso")
+        return redirect(url_for('auth.login'))
+    else:
+        buy: Buy = Buy.query.filter_by(user_id=session.get(
+        ), product_id=id).first_or_404("Voce não possui essa compra")
+        
+        if quant >= buy.n_items:
+            quant = buy.n_items
+            db.session.delete(buy)
+        else:
+            buy.n_items -= quant
+        
+        buy.user.n_items -= quant
+        buy.user.subtotal -= quant * buy.product.price
+        buy.product.stock += quant
+
+        db.session.commit()
+        
+        flash("Produto removido do seu carinho")
+        return redirect(url_for('store.showcase'))
+
+            
