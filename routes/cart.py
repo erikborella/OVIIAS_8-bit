05b0e18b5977e49950dd245dcbfc33f845d3c924
Erik Borella <erikborella@gmail.com>
2019-11-13 13:53:42 -0300

    add_to_cart now is a POST route

--- routes/cart.py
+++ routes/cart.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, redirect, url_for, flash, render_template
+from flask import Blueprint, redirect, url_for, flash, request
 from extensions import db
-
 from models import Product, User, Buy
 
 import session
@@ -8,39 +7,47 @@ import session
 cart = Blueprint('cart', __name__)
 
 
-@cart.route("/cart/add/<int:id>/<int:quant>")
-def add_to_cart(id: int, quant: int):
+@cart.route('/cart/add', methods=['POST'])
+def add_to_cart():
     if not session.is_logged():
         flash("Voce precisa estar logado para fazer isso")
         return redirect(url_for('auth.login'))
     else:
-        product: Product = Product.query.filter_by(
-            id=id).first_or_404("Produto nao encontrado")
-        user: User = User.query.filter_by(
-            id=session.get()).first_or_404("Usuario invalido")
+        product_id = int(request.form['product_id'])
+        quant = int(request.form['quant'])
 
-        if product.stock <= 0 or quant > product.stock:
-            flash("Quantidade do produto indiponivel")
-            return redirect(url_for('store.product', id=id))
+        if not product_id or not quant:
+            flash("Informações faltantes")
+            return redirect(url_for('store.index'))
         else:
-            buy = Buy.query.filter_by(user_id=session.get(), product_id=id).first()
+            product: Product = Product.query.filter_by(
+                id=product_id).first_or_404("Produto não encontrado")
+            user: User = User.query.filter_by(
+                id=session.get()).first_or_404("Usuario invalido")
 
-            # Check if alredy have the buy with this product
-            if buy is None:
-                buy = Buy(user, product, quant)
-                db.session.add(buy)
+            if product.stock <= 0 or quant > product.stock:
+                flash("Quantidade do produto indisponivel")
+                return redirect(url_for('store.product', id=product_id))
             else:
-                buy.n_items += quant
+                buy: Buy = Buy.query.filter_by(
+                    user_id=session.get(), product_id=product_id).first()
 
-            user.n_items += quant
-            user.subtotal += product.price * quant
+                # Check if alredy have the buy with this product
+                if buy is None:
+                    buy = Buy(user, product, quant)
+                    db.session.add(buy)
+                else:
+                    buy.n_items += quant
 
-            product.stock -= quant
+                user.n_items += quant
+                user.subtotal += product.price * quant
 
-            db.session.commit()
+                product.stock -= quant
 
-            flash("Produto adicionado com sucesso ao carinho")
-            return redirect(url_for("store.showcase"))
+                db.session.commit()
+
+                flash("Produto adicionado com sucesso ao seu carinho")
+                return redirect(url_for('store.showcase'))
 
 
 @cart.route("/cart/remove/<int:id>/<int:quant>")
@@ -51,20 +58,18 @@ def remove_to_cart(id: int, quant: int):
     else:
         buy: Buy = Buy.query.filter_by(user_id=session.get(
         ), product_id=id).first_or_404("Voce não possui essa compra")
-        
+
         if quant >= buy.n_items:
             quant = buy.n_items
             db.session.delete(buy)
         else:
             buy.n_items -= quant
-        
+
         buy.user.n_items -= quant
         buy.user.subtotal -= quant * buy.product.price
         buy.product.stock += quant
 
         db.session.commit()
-        
+
         flash("Produto removido do seu carinho")
         return redirect(url_for('store.showcase'))
-
-            
